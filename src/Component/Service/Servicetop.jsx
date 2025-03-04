9d04c5f5e0686d53fd7c4a2603148919a32827ec
aarya moktan <aaryamoktan2055@gmail.com>
2025-03-04 00:14:06 -0800

    first commit

--- src/Component/Service/Servicetop.jsx
+++ src/Component/Service/Servicetop.jsx
@@ -28,6 +28,39 @@ ut laboreet dolore magna aliqua. Ut enim ad minim veniam.
             <button><a href="/contact">Contact Us</a></button>
         </div>
     </div>
+    <div className='bussiness'>
+        
+        <div className='buss1'>
+            <h1>Our business process road</h1>
+            <div className='us1'>
+                <div className='us11'>
+                    <h1>01.</h1>
+                </div>
+                <div className='us12'>
+                    <h3>Create an Idea</h3>
+                    <h6>What’s hard is to develop the habits that enable us to come up with great ideas.</h6>
+                </div>
+            </div>
+            <div className='us1'>
+                <div className='us11'><h1>02.</h1></div>
+                <div className='us12'>
+                    <h3>Complete the Project</h3>
+                    <h6>You can organize yourself and your team in endless ways.</h6>
+                </div>
+            </div>
+            <div className='us1'>
+                <div className='us11'><h1>03.</h1></div>
+                <div className='us12'>
+                    <h3>	
+                    Execution</h3>
+                    <h6>When it comes to motivating teams, awarding members for good performance.</h6>
+                </div>
+            </div>
+        </div>
+        <div className='buss1'>
+           <img src="https://www.shutterstock.com/image-photo/two-business-women-sit-desk-600nw-2299697419.jpg"/> 
+        </div>
+    </div>
     </>
   )
 }
